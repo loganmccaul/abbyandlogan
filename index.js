169f1eae521bec426f00a7996df8bdd886605d85
Logan McCaul <loganmccaul@gmail.com>
2023-04-28 15:59:26 -0500

    init-rsvp

--- index.js
+++ index.js
@@ -1,92 +1,52 @@
-/* Nav selection */
+const collage = document.querySelector('.collage');
+const collageImages = [...document.querySelectorAll('.polaroid')];
 
-const highlightAnchorNav = (anchor) => {
-  const initialSelectedNav = document.querySelector(`nav a[href='#${anchor}']`);
+const { height: collageHeight, width: collageWidth } = collage.getBoundingClientRect();
 
-  if (initialSelectedNav) {
-    initialSelectedNav.classList.add('selected');
+let activeImage;
+
+collageImages.forEach(img => {
+  const { height: imgHeight, width: imgWidth } = img.getBoundingClientRect();
+  img.style.transform = `translate( ${Math.random() * (collageWidth - imgWidth)}px, ${Math.random() * (collageHeight - imgHeight)}px)`;
+});
+
+const activateImage = (clientX, clientY, target) => {
+  collage.appendChild(target);
+
+  const [offsetX, offsetY] = target.style.transform.match(/(\d+\.\d+)|(\d+)/g);
+
+  activeImage = {
+    element: target,
+    posX:  clientX - offsetX,
+    posY: clientY - offsetY
   }
 }
 
-// Page load
-const initAnchor = window.location.hash.substr(1);
-highlightAnchorNav(initAnchor);
+const down = (evt) => {
+  evt.preventDefault();
 
-// Get all nav links and add underline on click
-const navLinks = [...document.querySelectorAll('nav a')];
-const clearNavSelection = () => {
-  navLinks.forEach(navLink => navLink.classList.remove('selected'));
+  if (collageImages.includes(evt.target)) {
+    activateImage(evt.clientX, evt.clientY, evt.target);
+  } else if (collageImages.includes(evt.target.parentElement)) {
+    activateImage(evt.clientX, evt.clientY, evt.target.parentElement);
+  };
 }
 
-navLinks.forEach(link => link.addEventListener('click', function() {
-  clearNavSelection();
-  this.classList.add('selected');
-}));
-
-const sections = [...document.querySelectorAll('section')];
-
-/* Section fade in */
-const sectionObserver = new IntersectionObserver((events) => {
-  events.forEach(event => {
-    if (event.isIntersecting) {
-      event.target.style.opacity = 1;
-    } else {
-      event.target.style.opacity = 0;
-    }
-  })
-}, {
-  threshold: [.01]
-});
-
-sections.forEach(section => sectionObserver.observe(section))
-
-/* Dot animation */
-const initScrollPosition = window.scrollY;
-const dot = document.getElementById("dot-animated");
-const dotTopPosition = dot.getBoundingClientRect().top + initScrollPosition;
-
-// init dot state if load on an anchor
-window.addEventListener('DOMContentLoaded', () => {
-  if (initScrollPosition > 0) {
-    dot.style.animationName = 'unset';
-    dot.style.transformOrigin = 'top'
-
-    if (initScrollPosition > dotTopPosition) {
-      dot.style.transform = `scale(${dotTopPosition / 3})`;
-      dot.style.borderRadius = `calc(50% - ${dotTopPosition / 30}%)`
-    } else {
-      dot.style.transform = `scale(${initScrollPosition / 3})`;
-      dot.style.borderRadius = `calc(50% - ${initScrollPosition / 30}%)`
-    }
+const up = () => {
+  if (activeImage) {
+    activeImage = null;
   }
-});
+}
 
-const sectionHeaders = [...document.querySelectorAll('section h2')];
-
-const main = document.querySelector('main');
-
-// hand dot grow and shrink on scroll
-document.addEventListener('scroll', () => {
-  const scrollPosition = window.scrollY;
-
-  if (scrollPosition === 0) {
-    dot.style.animationName = 'breath';
-    dot.style.transformOrigin = 'unset'
+const move = (evt) => {
+  if (activeImage) {
+    const x = evt.clientX - activeImage.posX;
+    const y = evt.clientY - activeImage.posY;
+    
+    activeImage.element.style.transform = `translate(${x}px, ${y}px)`;
   }
+}
 
-  if (scrollPosition > 0 && scrollPosition < dotTopPosition && scrollPosition / 5 > 1) {
-    dot.style.animationName = 'unset';
-    dot.style.transformOrigin = 'top'
-    dot.style.transform = `scale(${scrollPosition / 3})`;
-    dot.style.borderRadius = `calc(50% - ${scrollPosition / 30}%)`
-  }
-
-  if (scrollPosition > 714 && !main.style.backgroundColor) {
-    main.style.backgroundColor = '#0F3A6C';
-  }
-
-  
-  if (scrollPosition <= 714 && main.style.backgroundColor) {
-    main.style.backgroundColor = '';
-  }
-});
+collage.addEventListener('pointerdown', down);
+collage.addEventListener('pointerup', up);
+collage.addEventListener('pointermove', move);
